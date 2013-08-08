7b86cdac76d5f54bf7020132914445c4394c4dff
Robert Forkel <xrotwang@googlemail.com>
2013-08-08 09:45:04 +0200

    fixes

--- fabfile.py
+++ fabfile.py
@@ -1,18 +1,3 @@
-from fabric.api import task, hosts
-from fabric.contrib.console import confirm
+from clld.deploy import tasks
+tasks.init('wals3')
 
-from clld.deploy import config, util
-
-
-APP = config.APPS['wals3']
-
-
-@hosts('forkel@cldbstest.eva.mpg.de')
-@task
-def deploy_test():
-    util.deploy(APP, 'test')
-
-
-@task
-def deploy():
-    util.deploy(APP, 'production')
