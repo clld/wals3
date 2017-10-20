501c8aa0ccf4015ea73fe0488a3517972998a133
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2017-10-20 14:48:04 +0200

    update location and iso code of Rumsien
    - fix clld/wals-data#129

---
+++ migrations/versions/430f8e3d07bf_update_cos_location_and_iso.py
@@ -0,0 +1,68 @@
+# coding=ascii
+"""update cos location and iso
+
+Revision ID: 430f8e3d07bf
+Revises: 45055609733c
+Create Date: 2017-10-20 13:58:01.089000
+
+"""
+from __future__ import unicode_literals
+
+# revision identifiers, used by Alembic.
+revision = '430f8e3d07bf'
+down_revision = u'45055609733c'
+
+import datetime
+
+from alembic import op
+import sqlalchemy as sa
+
+# https://github.com/clld/wals-data/issues/129
+ID = 'cos'
+
+LAT_BEFORE, LON_BEFORE = (37.0, -122.0)
+
+LAT_AFTER, LON_AFTER = (36.83333333333333333, -121.75)
+
+ISO_BEFORE = 'cst'
+
+
+def upgrade():
+    conn = op.get_bind()
+    
+    l = sa.table('language', *map(sa.column, ['pk', 'updated', 'id', 'latitude', 'longitude']))
+    i = sa.table('identifier', *map(sa.column, ['pk', 'type', 'name']))
+    licols = ['created', 'updated', 'active', 'version', 'language_pk', 'identifier_pk']
+    li = sa.table('languageidentifier', *map(sa.column, licols))
+
+    update_latlon = sa.update(l, bind=conn)\
+        .where(l.c.id == sa.bindparam('id_'))\
+        .where(l.c.latitude == sa.bindparam('lat_before'))\
+        .where(l.c.longitude == sa.bindparam('lon_before'))\
+        .values(updated=sa.func.now(),
+                latitude=sa.bindparam('lat_after'), longitude=sa.bindparam('lon_after'))
+
+    lwhere = (l.c.id == sa.bindparam('id_'))
+    iwhere = sa.and_(i.c.type == 'iso639-3', i.c.name == sa.bindparam('iso'))
+
+    unlink_iso = sa.delete(li, bind=conn)\
+        .where(sa.exists().where(sa.and_(li.c.language_pk == l.c.pk, lwhere)))\
+        .where(sa.exists().where(sa.and_(li.c.identifier_pk == i.c.pk, iwhere)))
+
+    l_pk = sa.select([l.c.pk]).where(lwhere).as_scalar()
+    i_pk = sa.select([i.c.pk]).where(iwhere).as_scalar()
+
+    link_iso = sa.insert(li, bind=conn).from_select(licols,
+        sa.select([sa.func.now(), sa.func.now(), True, 1, l_pk, i_pk])
+        .where(~sa.exists()
+            .where(li.c.language_pk == l_pk)
+            .where(li.c.identifier_pk == i_pk)))
+
+    update_latlon.execute(id_=ID,
+                          lat_before=LAT_BEFORE, lon_before=LON_BEFORE,
+                          lat_after=LAT_AFTER, lon_after=LON_AFTER)
+    unlink_iso.execute(id_=ID, iso=ISO_BEFORE)
+
+
+def downgrade():
+    pass
