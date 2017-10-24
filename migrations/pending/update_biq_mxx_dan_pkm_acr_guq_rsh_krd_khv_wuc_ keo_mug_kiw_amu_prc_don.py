7288e0d0c8a961741cbcf2e0a387cc26ec73165e
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2017-10-24 13:07:32 +0200

    more work on migration

--- migrations/pending/update_biq_mxx_dan_pkm_acr_guq_rsh_krd_khv_wuc_ keo_mug_kiw_amu_prc_don.py
+++ migrations/pending/update_biq_mxx_dan_pkm_acr_guq_rsh_krd_khv_wuc_ keo_mug_kiw_amu_prc_don.py
@@ -1,5 +1,5 @@
 # coding=ascii
-"""update biq mxx dan pkm acr guq don
+"""update biq mxx dan pkm acr guq rsh krd khv wuc keo mug kiw amu prc don
 
 Revision ID: 
 Revises: 
@@ -22,6 +22,7 @@ ID_BEFORE_AFTER = {
     'biq': {  # https://github.com/clld/wals-data/issues/112
         'name': ('Bilaan', 'Bilaan (Koronadal)'),
         'iso': (['bps', 'smk'], 'bpr'),
+        # FIXME: drop bps smk intended?
     },
     'mxx': {  # https://github.com/clld/wals-data/issues/100
         'macroarea': (None, 'North America'),
@@ -30,18 +31,52 @@ ID_BEFORE_AFTER = {
     'dan': {  # https://github.com/clld/wals-data/issues/97
         'iso': (['daf'], 'dnj'),
     },
-    'pkm': {  # https://github.com/clld/wals-data/issues/88
+    'pkm': {  # https://github.com/clld/wals-data/issues/88 also in reassign_iso_codes_glottocodes_and_el.py
         'iso': (['pob', 'poh'], 'poh'),
+        # FIXME: drop pob intended?
     },
-    'acr': {  # https://github.com/clld/wals-data/issues/86
+    'aci': {  # https://github.com/clld/wals-data/issues/86 also in reassign_iso_codes_glottocodes_and_el.py
         'iso': (['acc', 'acr'], 'acr'),
     },
     'guq': {  # https://github.com/clld/wals-data/issues/80
         'iso': ([], 'cbd'),
     },
+    # https://github.com/clld/wals-data/issues/75
+    'rsh': {
+        'name': ('Rushan', 'Shughni'),
+    },
+    'krd': {
+        'iso': (['kmr'], 'ckb'),
+        # FIXME: also nort2641 -> cent1972?
+    },
+    'khv': {
+        'name': ('Khvarshi', ' Khwarshi'),
+    },
+    'wuc': {
+        'name': ('Wu (Changzhou)', 'Wu'),
+    },
+    'keo': {
+        'name': ("Ke'o", 'Keo'),
+    },
+    'mug': {
+        'name': ('Mugil', 'Bargam'),
+    },
+    'kiw': {
+        'name': ('Kiwai', ' Kiwai (Southern)'),
+        'iso': (['kiw', 'kjd'], 'kiw'),
+        # FIXME: also none -> nort2930?
+    },
+    'amu': {
+        'name': ("Yanesha'", 'Amuesha'),
+    },
+    'prc': {
+        'name': ('Parecis', 'Paresi'),
+        'latlon': ((-14.0, -57.0), (-14.0, -57.5)),
+    },
     'don': {  # https://github.com/clld/wals-data/issues/71
         'name': ('Dong', 'Dong (Southern)'),
-        'iso': (['doc', 'kmc'], 'doc'),
+        'iso': (['doc', 'kmc'], 'kmc'),
+        # FIXME: also none -> sout2741?
     },
 }
 
@@ -54,7 +89,7 @@ def ascii_name(s, _whitelist=set(string.ascii_lowercase + ' ()0123456789')):
 def upgrade():
     conn = op.get_bind()
 
-    l = sa.table('language', *map(sa.column, ['pk', 'updated', 'id', 'name']))
+    l = sa.table('language', *map(sa.column, ['pk', 'updated', 'id', 'name', 'latitude', 'longitude']))
     w = sa.table('walslanguage', *map(sa.column, ['pk', 'ascii_name', 'macroarea']))
     icols = ['created', 'updated', 'active', 'version', 'id', 'type', 'description', 'lang', 'name']
     i = sa.table('identifier', *map(sa.column, ['pk'] + icols))
@@ -85,6 +120,12 @@ def upgrade():
 
     delete_i = i.delete(bind=conn).where(iotherwhere)\
         .where(~sa.exists().where(li.c.identifier_pk == i.c.pk))
+
+    update_latlon = l.update(bind=conn).where(lwhere)\
+        .where(l.c.latitude == sa.bindparam('lat_before'))\
+        .where(l.c.longitude == sa.bindparam('lon_before'))\
+        .values(updated=sa.func.now(),
+            latitude=sa.bindparam('lat_after'), longitude=sa.bindparam('lon_after'))
 
     add_lang_ma = l.update(bind=conn).where(lwhere)\
         .where(sa.exists().where(l.c.pk == w.c.pk)
@@ -127,6 +168,10 @@ def upgrade():
             update_wals.execute(id_=id_, ascii_before=ascii_name(before), ascii_name=ascii_name(after))
             delete_li.execute(id_=id_, after=after)
             delete_i.execute(id_=id_, after=after)
+        if 'latlon' in fields:
+            (lat_before, lon_before), (lat_after, lon_after) = fields['latlon']
+            update_latlon.execute(id_=id_, lat_before=lat_before, lon_before=lon_before,
+                                  lat_after=lat_after, lon_after=lon_after)
         if 'macroarea' in fields:
             before, after = fields['macroarea']
             assert before is None
