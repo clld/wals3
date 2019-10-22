93697ac488bbacdea213b9486d56fea2ac3b5f31
xrotwang <xrotwang@googlemail.com>
2019-10-22 08:30:31 +0200

    fixed blog feed

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     install_requires=[
         'clldutils>=3.0.1',
         'clldmpg>=3.4.0',
-        'clld>=5.0.0',
+        'clld>=5.0.1',
         'BeautifulSoup4',
         'feedparser',
         'sqlalchemy',
